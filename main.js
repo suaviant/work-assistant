d7b58df075f453e234898ed47747325918891a55
braithwaite_devon_tech-flag <devon.braithwaite@tech-flag.co.jp>
2026-06-22 18:46:07 +0900

    blocker code working

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-const { app, BrowserWindow} = require('electron/main');
+const { app, BrowserWindow, globalShortcut} = require('electron/main');
 const { ipcMain } = require('electron');
 const path = require('node:path');
 const focusManager = require('./focusManager');
@@ -6,27 +6,54 @@ const focusManager = require('./focusManager');
 console.log('Hello from Electron')
 
 
-ipcMain.on('focus-mode', ()=>{
-	focusManager.enterFocusMode();
-})
 
 console.log(__dirname);
 console.log(path.join(__dirname, 'preload.js'));
 
+let mainWindow;
+
 const createWindow = () => {
-	const win = new BrowserWindow({
+	mainWindow = new BrowserWindow({
 		width: 800,
 		height: 600,
 		webPreferences:{
 			preload: path.join(__dirname, 'preload.js')
-		}
+		},
+
+		frame: false,
+		titleBarStyle: 'hidden',
+		transparent: false,
+
+		skipTaskbar: false,
+		resizable: false
+
 	})
 	
-	win.loadFile('index.html')
-	win.webContents.openDevTools()
+	mainWindow.loadFile('index.html')
+	mainWindow.webContents.openDevTools()
 }
 
+ipcMain.on('focus-mode', ()=>{
+	focusManager.enterFocusMode();
+	mainWindow.setAlwaysOnTop(true, "screen-saver");
+	mainWindow.show();
+	mainWindow.focus();
+	mainWindow.moveTop();
+})
+
+
 app.whenReady().then(() => {
+	globalShortcut.register('Escape', ()=>{
+		//console.log("ESC");
+		//app.quit();
+		focusManager.exitFocusMode();
+		mainWindow.setAlwaysOnTop(false);
+	});
+
+	globalShortcut.register('Ctrl+Shift+Q', () => {
+		app.quit();
+	})
+
 	createWindow()
 	
 	app.on('activate', () => {
