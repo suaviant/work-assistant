59e917dde7b82d3a1290c72e23b7913fbca0c5ec
braithwaite_devon_tech-flag <devon.braithwaite@tech-flag.co.jp>
2026-06-22 15:09:35 +0900

    done button that moves through subtasks

--- renderer.js
+++ renderer.js
@@ -1,2 +1,27 @@
-const information = document.getElementById('info')
-information.innerText = `This app is using Chrome (v${versions.chrome()}), Node.js (v${versions.node()}), and Electron (v${versions.electron()})`
+const task = {
+    steps: [
+        "Determine budget",
+        "Identify interests",
+        "Shortlist three gifts",
+        "Compare prices",
+        "Purchase"
+    ],
+    currentStep: 2
+}
+
+function updateUI(){
+    document.getElementById("objective").innerText = task.steps[task.currentStep];
+
+    document.getElementById("progress").innerText = 
+    `Progress ${task.currentStep+1}/${task.steps.length}`;
+}
+
+updateUI();
+
+document.getElementById("doneButton")
+.addEventListener("click", ()=> {
+    if (task.currentStep < task.steps.length -1){
+        task.currentStep++;
+    }
+    updateUI();
+})
